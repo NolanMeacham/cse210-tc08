4a96e7a62ad182fde828850417df3e80dc55387e
Nolan Meacham <nolan.meacham@gmail.com>
2021-02-26 14:37:24 -0700

    copied classes from rfk game

---
+++ batter/game/draw_actors_action.py
@@ -0,0 +1,32 @@
+from game.action import Action
+
+class DrawActorsAction(Action):
+    """
+
+    Stereotype:
+        Controller
+
+    Attributes:
+        _output_service (OutputService): An instance of OutputService
+
+    """
+    def __init__(self, output_service):
+        """
+        Class constructor.
+
+        Args:
+            output_service: an instance of Output_Service class.
+
+        """
+        self._output_service = output_service
+
+    def execute(self, cast):
+        """
+        Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}
+        """
+        self._output_service.clear_screen()
+
+    
